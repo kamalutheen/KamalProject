b522bb6b53e8215a91e6cd6bd98eaaed7cf41c1f
kamalutheen <kamalutheen12@gmail.com>
2019-01-24 12:16:23 +0530

    I have added new class

---
+++ src/package01/BaseClass.java
@@ -0,0 +1,10 @@
+package package01;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my base class");
+	}
+
+}
